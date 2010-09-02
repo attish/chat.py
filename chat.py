8c9c19909437d462fb00cd3fc57f67239944b7e9
Attila Nagy <nagy.attila.1984@gmail.com>
2010-09-02 03:01:32 +0200

    chat.py: frame to allow almost normal usage
    [frame]
    
    Several usability caveats remain.
    
    A more serious issue: if a new window is opened, existing chat is
    loaded one line per second. The problem is that it is loaded one line
    per long poll.

--- chat.py
+++ chat.py
@@ -4,6 +4,7 @@ import web as webpy
 import threading
 import time
 import datetime
+import random
 
 # messages is the list of message lines. thread_lock is a dict that
 # assigns locks to waiting threads. session_pos is a dict that assigns
@@ -14,7 +15,9 @@ session_pos = {}
 
 urls = (
     '/([0-9]+)', 'Read',
-    '/say/(.*)', 'Say',
+    '/', 'KeepReading',
+    '/say', 'Say',
+    '/chat', 'Frame',
     )
 
 class Read:
@@ -31,12 +34,72 @@ class Read:
         yield messages[pos] + '\n'
         session_pos[session_id] += 1
 
+class KeepReading:
+    def GET(self):
+        webpy.header('Content-type', 'text/html')
+        thread_id = str(threading.current_thread())
+        pos = 0
+        while True:
+            if pos == len(messages):
+                thread_lock[thread_id] = threading.Event()
+                thread_lock[thread_id].clear()
+                thread_lock[thread_id].wait()
+            yield messages[pos] + '\n'
+            pos += 1
+
 class Say:
-    def GET(self, line):
+    def GET(self):
+        line = webpy.input()['l']
         messages.append(line)
         for thread in thread_lock:
             thread_lock[thread].set()
         return "Line '%s' accepted." % line
+
+class Frame:
+    def GET(self):
+        input = webpy.input()
+        if 'l' in input:
+            line = input['l']
+            messages.append(line)
+            for thread in thread_lock:
+                thread_lock[thread].set()
+        randnum = random.randint(0, 2000000000)
+        page = """
+        <html>
+            <head>
+                <title>Minimal web chat</title>
+                <script type="text/javascript" src="static/jquery.js"></script>
+            </head>
+            <body>
+                <div id="chat"></div>
+                    <input id="text">
+                    </input>
+                    <input type="button" value="Send" onclick="sendMsg()">
+                    </input>
+                </form>
+                <script type="text/javascript">
+                    function sendMsg() {
+                        var msg = $('#text').val();
+                        $.get('/say?l=' + escape(msg));
+                    }
+
+                    function getMsg() {
+                        $.ajax({
+                            url: '/%d',
+                            dataType: 'text',
+                            type: 'get',
+                            success: function(line) {
+                                $('#chat').append('<p>' + line + '</p>');
+                                setTimeout('getMsg()', 1000);
+                                }
+                        });
+                    }
+                    getMsg();
+                </script>
+            </body>
+        </html>
+        """
+        return page % randnum
 
 if __name__ == '__main__':
     webapp = webpy.application(urls, globals())
